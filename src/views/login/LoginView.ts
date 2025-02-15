cf957c1821252aa6fe709de04ff0c4c0c369c34b
Logan McNatt <lmcnatt@nex.io>
2025-02-14 22:55:11 -0700

    Updated frontend login

--- src/views/login/LoginView.ts
+++ src/views/login/LoginView.ts
@@ -3,13 +3,54 @@ export default {
 	emits: ["authenticate"],
 	data: function () {
 		return {
-			isAuthenticated: false
+			isAuthenticated: false,
+			alertType: "error",
+			errorMsg: "",
+			password: "",
+			email: "",
+			dialog: false,
+			isLoading: false,
+			emailRules: [
+				(value) => !!value || "Required.",
+				(value) => (value && value.length >= 3) || "Min 3 characters"
+			],
+			passwordRules: [
+				(value) => !!value || "Required.",
+				(value) => (value && value.length >= 8) || "Min 8 characters"
+			],
+			isFormValid: false,
+			hardCodedEmailForDemo: "wizards@gmail.com",
+			hardCodedPasswordForDemo: "15bagels"
 		}
 	},
 	methods: {
 		submitLogin() {
-			this.isAuthenticated = true
-			this.$emit("authenticate", this.isAuthenticated)
+			if (!this.isFormValid) {
+				return
+			}
+
+			this.errorMsg = ""
+			if (
+				this.hardCodedEmailForDemo === this.email &&
+				this.hardCodedPasswordForDemo === this.password
+			) {
+				this.alertType = "success"
+				this.errorMsg = "Welcome Back Master Wizard!"
+				this.isLoading = true
+				setTimeout(() => {
+					this.isAuthenticated = true
+					this.$emit("authenticate", this.isAuthenticated)
+				}, 1000)
+			} else if (this.email === this.password) {
+				this.alertType = "warning"
+				this.errorMsg = "Your username and password can't be the same!"
+			} else {
+				this.alertType = "error"
+				this.errorMsg = "Login failed! Try again."
+			}
+		},
+		forgotPassword() {
+			console.log("here")
 		}
 	}
 }
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,7 +1,68 @@
 <template>
-	<button v-if="!isAuthenticated" @click="submitLogin()">
-		Login Page Placeholder Here
-	</button>
+	<div class="login-page-content">
+		<div class="login-box">
+			<h1 class="logo-area">Login</h1>
+			<div class="input-container">
+				<v-form v-model="isFormValid">
+					<div class="input-container">
+						<v-text-field
+							v-model="email"
+							label="Email"
+							:rules="emailRules"
+							hide-details="auto"
+						></v-text-field>
+
+						<v-text-field
+							v-model="password"
+							label="Password"
+							type="password"
+							:rules="passwordRules"
+							hide-details="auto"
+							@keyup.enter="submitLogin()"
+						></v-text-field>
+
+						<v-alert
+							v-if="errorMsg !== ''"
+							:type="alertType"
+							@click="errorMsg = ''"
+							>{{ errorMsg }}</v-alert
+						>
+					</div>
+
+					<div class="button-container">
+						<v-btn
+							v-if="!isAuthenticated"
+							:loading="isLoading"
+							:disabled="!isFormValid"
+							@click="submitLogin()"
+						>
+							Login Here
+							<div></div>
+							<div></div>
+						</v-btn>
+						<v-btn @click="dialog = true"> Forgot Password </v-btn>
+						<v-dialog v-model="dialog" :scrim="false" persistent>
+							<v-card>
+								<v-card-text>
+									<v-text-field
+										v-model="email"
+										label="Password Reset Email"
+										:rules="emailRules"
+										hide-details="auto"
+									>
+									</v-text-field>
+								</v-card-text>
+								<v-card-actions>
+									<v-btn variant="text" @click="dialog = false"> Close </v-btn>
+									<v-btn variant="text"> Submit </v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-dialog>
+					</div>
+				</v-form>
+			</div>
+		</div>
+	</div>
 </template>
 <script src="./LoginView.ts" />
 
