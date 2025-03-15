5a6d813fb69c02ea94c01e3e8e44be19e4794efb
Logan McNatt <lmcnatt@nex.io>
2025-03-15 12:23:21 -0600

    Profile settings

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,63 @@ export default {
 		RouterLink,
 		RouterView
 	},
-
-	computed: mapState({
-		authUser() {
-			return this.$store.state.auth.user
-		},
-		isAuthenticated() {
-			return (
-				this.$store.state.auth.status.loggedIn &&
-				this.authUser.token !== undefined
-			)
-		},
-		title() {
-			return "Welcome " + this.authUser.name + "!"
+	data: function () {
+		return {
+			profileDialog: false,
+			profileIsUploading: false,
+			verificationEmailLoading: false,
+			showEmailNotVerifiedDialog: false,
+			showChangeEmailTextField: false,
+			changeEmail: false,
+			successVerificationMessage: "",
+			changeEmailRules: [
+				(value) => !!value || "Required.",
+				(value) => (value && value.length >= 3) || "Min 3 characters"
+			],
+			profile: {
+				avatar: "",
+				name: "",
+				title: "",
+				icon: "mdi-account-circle",
+				color: "info"
+			},
+			profilePictureImage: "",
+			emailOfVerification: ""
 		}
-	}),
+	},
+	computed: {
+		...mapState({
+			user() {
+				return this.$store.state.user.user
+			},
+			auth() {
+				return this.$store.state.auth
+			},
+			authUser() {
+				return this.auth.user
+			},
+			isAuthenticated() {
+				return this.auth.status.loggedIn && this.authUser.token !== undefined
+			},
+			title() {
+				return "Welcome " + this.authUser.name + "!"
+			},
+			avatarURL() {
+				return this.auth.user.avatar
+			},
+			profilePictureChangeLabel() {
+				return "Change Profile Picture"
+			}
+		})
+	},
 	updated() {
 		if (this.isAuthenticated) {
 			this.$router.push({ name: "home" })
+		}
+	},
+	created() {
+		if (this.authUser) {
+			this.getCurrentUser()
 		}
 	},
 	methods: {
@@ -43,6 +82,50 @@ export default {
 		},
 		checkAuth(auth) {
 			console.log("Authenticated!", auth)
+		},
+		onAvatarChange(e) {
+			var image = e.target.files || e.dataTransfer.files
+
+			if (!image.length) return
+			this.profileIsUploading = true
+			this.$store
+				.dispatch("user/uploadAvatar", image[0], { root: true })
+				.then((response) => {
+					this.$store.commit("auth/uploadAvatarSuccess", response.avatar)
+					this.profileIsUploading = false
+				})
+				.catch((error) => {
+					console.log(error)
+					alert("Error. Try again")
+					this.profileIsUploading = false
+				})
+		},
+		removeAvatar() {
+			this.profileIsUploading = true
+			this.$store
+				.dispatch("user/removeAvatar")
+				.then((response) => {
+					this.$store.commit("auth/uploadAvatarSuccess", response.avatar)
+					this.profileIsUploading = false
+				})
+				.catch((error) => {
+					console.log(error)
+					alert("Error. Try again")
+					this.profileIsUploading = false
+				})
+		},
+		getCurrentUser() {
+			this.profile.name = this.authUser.name
+
+			this.profile.title = this.title
+			this.$store.dispatch("user/getUser").then((response) => {
+				if (response.avatar) {
+					this.$store.commit("auth/uploadAvatarSuccess", response.avatar)
+				}
+				if (!response.email_verified_at) {
+					this.showEmailNotVerifiedDialog = true
+				}
+			})
 		}
 	}
 }
@@ -56,14 +139,45 @@ export default {
 			<v-spacer></v-spacer>
 			<v-btn to="home" default>Home</v-btn>
 			<v-btn to="about">About</v-btn>
-			<v-btn
-				:prepend-icon="
-					theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
-				"
-				@click="changeTheme"
-				>Toggle Theme
-			</v-btn>
-			<v-btn @click="logout()">Logout</v-btn>
+			<v-menu min-width="200px" rounded>
+				<template v-slot:activator="{ props }">
+					<v-btn icon v-bind="props">
+						<v-avatar color="brown" size="large">
+							<v-img
+								icon
+								v-bind="props"
+								v-if="avatarURL"
+								alt="Avatar"
+								:src="avatarURL"
+							></v-img>
+							<v-icon
+								v-bind="props"
+								v-else
+								:color="profile.color"
+								:icon="profile.icon"
+							></v-icon>
+						</v-avatar>
+					</v-btn>
+				</template>
+				<v-card>
+					<v-card-text>
+						<div class="mx-auto text-center">
+							<h3>{{ profile.name }}</h3>
+							<v-divider class="my-3"></v-divider>
+							<v-btn
+								:prepend-icon="
+									theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+								"
+								@click="changeTheme"
+								>Toggle Theme
+							</v-btn>
+							<v-btn @click="profileDialog = true">Profile</v-btn>
+							<v-divider class="my-3"></v-divider>
+							<v-btn @click="logout()">Logout</v-btn>
+						</div>
+					</v-card-text>
+				</v-card>
+			</v-menu>
 		</v-app-bar>
 
 		<v-main>
@@ -77,6 +191,39 @@ export default {
 					@authenticate="checkAuth($event)"
 				/>
 			</v-container>
+
+			<v-dialog v-model="profileDialog">
+				<v-form>
+					<v-card>
+						<v-card-title>Profile</v-card-title>
+						<v-card-subtitle>Enter your profile options here</v-card-subtitle>
+						<v-card class="mx-auto" min-width="300" max-width="500" rounded="0">
+							<v-img
+								cover
+								v-if="avatarURL"
+								:src="avatarURL"
+								alt="Avatar"
+							></v-img>
+							<v-icon
+								v-else
+								:color="profile.color"
+								:icon="profile.icon"
+							></v-icon>
+
+							<v-file-input
+								accept="image/*"
+								:loading="profileIsUploading"
+								:disabled="profileIsUploading"
+								@change="onAvatarChange"
+								:label="profilePictureChangeLabel"
+							></v-file-input>
+						</v-card>
+						<v-card-actions>
+							<v-btn @click="removeAvatar">Remove Profile Picture</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-form>
+			</v-dialog>
 		</v-main>
 	</v-app>
 </template>
