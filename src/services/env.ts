0135555b2f3028920f31ddd4403a1df673131580
Logan McNatt <lmcnatt@nex.io>
2025-04-14 17:58:16 -0600

    8888

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,4 +1,4 @@
-let API_URL = "http://54.165.180.0:8000/api/"
+let API_URL = "http://54.165.180.0:8888/api/"
 if (import.meta.env.MODE === "development") {
 	API_URL = "http://127.0.0.1:8000/api/"
 }
